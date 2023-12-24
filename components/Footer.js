c9d68c7351b9db53ba1ec6c7012090a02acce8b1
satoru <satoru.main0301create@gmail.com>
2023-12-24 17:10:00 +0900

    🎨 Footer部分とLinkの部分をコンポーネント化

---
+++ components/Footer.js
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+
+export function Footer() {
+    return (
+        <div>
+            <a
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Thanks By{' '}
+                <Image
+                    src="/vercel.svg"
+                    alt="Vercel Logo"
+                    className={styles.vercelLogo}
+                    width={100}
+                    height={24}
+                    priority
+                />
+            </a>
+        </div>
+    )
+}
