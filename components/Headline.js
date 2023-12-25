b5a183528c6417b187ae1cb752daaea7e0d315ae
satoru-github <satoru.main0301create@gmail.com>
2023-12-25 17:22:07 +0900

    🎨 メイン部分のコンポーネント化

--- components/Headline.js
+++ components/Headline.js
@@ -2,15 +2,15 @@
 import styles from '@/styles/Home.module.css'
 
 export function Headline(props) {
-    console.log(props.title);
     return (
         <div>
             <h1 className={styles.title}>{props.page} page</h1>
             <div className={styles.description}>
                 <p>
                     Get started by editing&nbsp;
-                    <code className={styles.code}>pages/{props.page}.js</code>
+                    {props.children}
                 </p>
+                <button onClick={props.onClick}>ボタン</button>
             </div>
         </div>
     )
