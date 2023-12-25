c461a2eecef1d3b6be9512b9b05313ae44a7c901
satoru <satoru.main0301create@gmail.com>
2023-12-26 00:49:25 +0900

    ✨ ヘッダーの追加

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { Footer } from '@/components/Footer'
 import { Main } from '@/components/Main'
 import styles from '@/styles/Home.module.css'
+import { Header } from '@/components/Header'
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <title>Create Next App_index</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header></Header>
       <Main page="index" />
       <Footer />
     </div>
