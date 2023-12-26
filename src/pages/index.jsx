e73902b4897d53b4dedfea4dda2096fdfd9983a3
satoru-github <satoru.main0301create@gmail.com>
2023-12-26 17:15:54 +0900

    ✨ カウンターを設置してuseStateでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import { Footer } from '@/src/components/Footer'
 import { Main } from '@/src/components/Main'
 import styles from '@/src/styles/Home.module.css'
 import { Header } from '@/src/components/Header'
-import { useCallback, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 // const handleClick = (e) => {
 //   console.log(e.target.href);
@@ -11,19 +11,24 @@ import { useCallback, useEffect } from 'react'
 // }
 
 export default function Home() {
-  const foo = 1;
+  const [count, setCount] = useState(1)
+  // let foo = 1;
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  // const handleClick = useCallback((e) => {
+  //   console.log(e.target.href);
+  //   e.preventDefault();
+  //   alert(foo);
+  // }, []);
+  
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
 
   useEffect(() => {
     console.log("maunt");
     document.body.style.backgroundColor = "lightblue";
     return () => {
-      console.log("unmaunt");
       document.body.style.backgroundColor = "";
     }
   }, [])
@@ -34,12 +39,13 @@ export default function Home() {
         <title>Create Next App_index</title>
       </Head>
       <Header></Header>
-      <a
+      <h1>{count}</h1>
+      <button
         href='/about'
         onClick={handleClick}
       >
         ボタン
-      </a>
+      </button>
       <Main page="index" />
       <Footer />
     </div>
