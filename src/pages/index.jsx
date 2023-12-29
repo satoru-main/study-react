72ad3fcc6030e9a10a90fecc8d750d3bb152a809
satoru <satoru.main0301create@gmail.com>
2023-12-29 22:04:07 +0900

    ✨ useStateで文字列と真偽値について解説

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import { Footer } from '@/src/components/Footer'
 import { Main } from '@/src/components/Main'
 import styles from '@/src/styles/Home.module.css'
 import { Header } from '@/src/components/Header'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 // const handleClick = (e) => {
 //   console.log(e.target.href);
@@ -11,12 +11,26 @@ import { useEffect, useState } from 'react'
 // }
 
 export default function Home() {
-  const [count, setCount] = useState(1)
-  
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("a");
+  const [isShow, setIsShow] = useState(true);
+
   const handleClick = (e) => {
     setCount((count) => count + 1);
     setCount((count) => count + 1);
   };
+
+  const handleDisplay = (e) => {
+      setIsShow((isShow) => !isShow);
+  }
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字までにしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
 
   useEffect(() => {
     console.log("maunt");
@@ -32,15 +46,16 @@ export default function Home() {
         <title>Create Next App_index</title>
       </Head>
       <Header></Header>
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
       <button
-        href='/about'
-        onClick={handleClick}
-      >
-        ボタン
+        onClick={handleDisplay}>
+        {isShow ? "非表示" : "表示"}
       </button>
+
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
-    </div>
-  )
+    </div >
+  );
 }
