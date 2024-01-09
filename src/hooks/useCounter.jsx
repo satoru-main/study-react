9df7337e80fd95160da3a50eed1e45c9124d468e
satoru-github <satoru.main0301create@gmail.com>
2024-01-09 14:35:01 +0900

    ✨ usememoについて学んだ

--- src/hooks/useCounter.jsx
+++ src/hooks/useCounter.jsx
@@ -1,22 +1,22 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 
 export const useCounter = () => {
-    const [count, setCount] = useState(1);
-    const [isShow, setIsShow] = useState(true);
+  const [count, setCount] = useState(1);
+  const [isShow, setIsShow] = useState(true);
 
-    const doubleCount = count * 2;
+  const doubleCount = useMemo(() => {
+    return count * 2;
+  }, [count]);
 
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  }, [count]);
 
+  const handleDisplay = useCallback(() => {
+    setIsShow((prevIsShow) => !prevIsShow);
+  }, []);
 
-    const handleClick = useCallback(() => {
-      if (count < 10) {
-        setCount((prevCount) => prevCount + 1);
-      }
-    }, [count]);
-  
-    const handleDisplay = useCallback(() => {
-      setIsShow((prevIsShow) => !prevIsShow);
-    }, []);
-  
-    return { count, doubleCount, isShow, handleClick, handleDisplay }
-  }
+  return { count, doubleCount, isShow, handleClick, handleDisplay }
+}
